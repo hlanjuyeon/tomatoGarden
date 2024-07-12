d3e55abcc86435f94e92eba1075a1efc29732437
Han Juyeon <tomato.202404.hanjy@gmail.com>
2024-07-12 13:29:31 +0900

    Fix: error

--- src/app/main/apps/expense/expenseTab/ExpenseAddTab.tsx
+++ src/app/main/apps/expense/expenseTab/ExpenseAddTab.tsx
@@ -1,10 +1,18 @@
 function ExpenseAddTab() {
 
 
-    return (
-        <>
-ExpenseAddTab
-        </>
+    return
+    (
+        <div className="flex w-full justify-center items-center">
+            <div className="h-full px-128">
+                <span className="flex w-full justify-center items-center">
+                    <h1 className="text-black text-3xl font-semibold">2024年 06月</h1>
+                </span>
+                <div className="flex w-full h-22 m-auto py-6 justify-center items-center rounded-lg bg-[#157B3E] text-white text-xl font-semibold">
+                    <div className="">承認待ち</div>
+                </div>
+            </div>
+        </div >
     )
 }
 
